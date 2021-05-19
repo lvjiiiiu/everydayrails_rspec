27308c018215477dc76f166de64927613fd152e1
lvjiiiiu <lv_jiiiiu@yahoo.co.jp>
2021-05-19 17:38:28 +0900

    project_spec 修正

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -44,4 +44,28 @@ RSpec.describe Project, type: :model do
 
     expect(other_project).to be_valid
   end
+
+  describe "late status" do
+    # 締め切り日が過ぎていれば遅延していること
+    it "is late when the due date is past today" do
+      project = FactoryBot.create(:project_due_yesturday)
+      expect(project).to be_late
+    end
+
+    # 締め切り日が今日ならスケジュール通りであること
+    it "is on time when the due date is today" do
+      project = FactoryBot.create(:project_due_today)
+      expect(project).to_not be_late
+    end
+
+    # 締め切り日が未来ならスケジュール通りであること
+    it "is on time when the due date is in the future" do
+      project = FactoryBot.create(:project_due_tomorrow)
+      expect(project).to_not be_late
+    end
+  end
+
+
+
+
 end
