c4dff8bec0c97bbd3a1ffbc84bf4d22aadc22330
lvjiiiiu <lv_jiiiiu@yahoo.co.jp>
2021-05-20 14:49:36 +0900

    演習問題1

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -3,41 +3,25 @@ require 'rails_helper'
 RSpec.describe Project, type: :model do
   # ユーザー単位では重複したプロジェクト名を許可しないこと
   it "does not allow duplicate project names per user" do
-    user = User.create(
-      first_name: "Joe",
-      last_name: "Tester",
-      email: "joetester@example.com",
-      password: "dottle-nouveau-pavilion-tights-furze",
-    )
+    user = FactoryBot.create(:user)
     user.projects.create(
       name: "Test Project",
     )
-
     new_project = user.projects.build(
       name: "Test Project",
     )
-
     new_project.valid?
     expect(new_project.errors[:name]).to include("has already been taken")
   end
 
+  # 異なるユーザーであれば重複したプロジェクト名を許可すること
   it "allows two users to share a project name" do
-    user = User.create(
-      first_name: "Joe",
-      last_name: "Tester",
-      email: "joetester@example.com",
-      password: "dottle-nouveau-pavilion-tights-furze",
-    )
+    user = FactoryBot.create(:user)
     user.projects.create(
       name: "Test Project",
     )
 
-    other_user = User.create(
-      first_name: "Jane",
-      last_name: "Tester",
-      email: "janetester@example.com",
-      password: "dottle-nouveau-pavilion-tights-furze",
-    )
+    other_user = FactoryBot.create(:user)
     other_project = other_user.projects.build(
       name: "Test Project",
     )
@@ -69,10 +53,5 @@ RSpec.describe Project, type: :model do
       project = FactoryBot.create(:project, :with_notes)
       expect(project.notes.length).to eq 5
     end
-
   end
-
-
-
-
 end
