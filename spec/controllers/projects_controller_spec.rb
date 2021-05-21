6cf3d51c43d4f175e26b2f3098772a79193d6fd9
lvjiiiiu <lv_jiiiiu@yahoo.co.jp>
2021-05-21 09:32:37 +0900

    ログインをシミュレート

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -2,14 +2,19 @@ require 'rails_helper'
 
 RSpec.describe ProjectsController, type: :controller do
   describe "#index" do
+    before do
+      @user = FactoryBot.create(:user)
+    end
     # 正常にレスポンスを返すこと
     it "responds succesfilly" do
+      sign_in @user
       get :index
       expect(response).to be_success
     end
 
     # 200レスポンスを返すこと
     it "return a 200 response" do
+      sign_in @user
       get :index
       expect(response).to have_http_status "200"
     end
