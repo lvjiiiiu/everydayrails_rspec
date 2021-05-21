ee6dbbfffc594dff62ee8f62d9fb3c245e727790
lvjiiiiu <lv_jiiiiu@yahoo.co.jp>
2021-05-21 15:29:47 +0900

    一度保存

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -160,6 +160,71 @@ RSpec.describe ProjectsController, type: :controller do
         expect(response).to redirect_to "/users/sign_in"
       end
     end
-
   end
+
+  # describe "#destory" do
+  #   #認可されたユーザーとして
+  #   context "as an authorized user" do
+  #     before do
+  #       @user = FactoryBot.create(:user)
+  #       @project = FactoryBot.create(:project, owner: @user)
+  #     end
+
+  #     # プロジェクトを削除できること
+  #     it "deletes a project" do
+  #       sign_in @user
+  #       expect {
+  #       delete :destroy, params: { id: @project.id }
+  #       }.to change(@user.projects, :count).by(-1)
+  #     end
+  #   end
+
+  #   # 認可されていないユーザーとして
+  #   context "as an unauthorized user" do
+  #     before do
+  #       @user = FactoryBot.create(:user)
+  #       other_user = FactoryBot.create(:user)
+  #       @project = FactoryBot.create(:project, owner: other_user)
+  #     end
+
+  #     # プロジェクトを削除できないこと
+  #     it "does not delete the project" do
+  #       sign_in @user
+  #       expect {
+  #       delete :destroy, params: { id: @project.id }
+  #       }.to_not change(Project, :count)
+  #     end
+
+  #     # ダッシュボードへリダイレクトすること
+  #     it "redirect to the dashboard" do
+  #       sign_in @user
+  #       delete :destroy, params: { id: @project.id }
+  #       expect(response).to redirect_to root_path
+  #     end
+  #   end
+
+    # # ゲストとして
+    # context "as a guest" do
+    #   @project = FactoryBot.create(:project)
+      
+    #   # 302レスポンスを返すこと
+    #   it "returns a 302 respnse" do
+    #     delete :destroy, params: { id: @project.id }
+    #     expect(response).to have_http_status "302"
+    #   end
+
+    #   # サインイン画面にリダイレクトすること
+    #   it "redirect to the sign-in page" do
+    #     delete :destroy, params: { id: @project.id }
+    #     expect(response).to redirect_to "/users/sign_in"
+    #   end
+
+    #   # プロジェクトを削除できないこと
+    #   it "does not delete the project" do
+    #     expect {
+    #       delete :destroy, params: { id: @project.id }
+    #     }.to_not change(Project, count)
+    #   end
+    # end
+  # end
 end
