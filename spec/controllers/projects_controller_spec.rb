7b3a0e73c4df63f0969a8ea33babd3a9d06ddcf7
lvjiiiiu <lv_jiiiiu@yahoo.co.jp>
2021-05-23 16:36:13 +0900

    projects_controller_spec editアクション追加

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -84,7 +84,35 @@ RSpec.describe ProjectsController, type: :controller do
     end
   end
 
-  
+  describe "#edit" do
+    # 認可されたユーザーとして
+    context "as an authorizes user" do
+      before do
+        @user = FactoryBot.create(:user)
+        @project = FactoryBot.create(:project, owner: @user)
+      end
+      # 正常にレスポンスを返すこと
+      it "responds successfully" do
+        sign_in @user
+        get :edit, params: { id: @project.id }
+        expect(response).to be_success
+      end
+    end
+
+    context "as an unauthorized user" do
+      before do
+        @user = FactoryBot.create(:user)
+        other_user = FactoryBot.create(:user)
+        @project = FactoryBot.create(:project, owner: other_user)
+      end
+      # ダッシュボードへリダイレクトすること
+      it "redirect to the dasheboard" do
+        sign_in @user
+        get :edit, params: { id: @project.id }
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
 
 
 
