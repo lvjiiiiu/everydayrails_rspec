a384b9653784a8539a2272b227516ef760c006d7
lvjiiiiu <lv_jiiiiu@yahoo.co.jp>
2021-05-25 18:38:28 +0900

    保存

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -8,15 +8,9 @@ RSpec.describe ProjectsController, type: :controller do
       end
       # 正常にレスポンスを返すこと
       it "responds succesfilly" do
-        sign_in @user
+        # sign_in @user
         get :index
         expect(response).to be_success
-      end
-
-      # 200レスポンスを返すこと
-      it "return a 200 response" do
-        sign_in @user
-        get :index
         expect(response).to have_http_status "200"
       end
     end
