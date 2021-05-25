df332c057782486a38bd16317d276a406746b54d
lvjiiiiu <lv_jiiiiu@yahoo.co.jp>
2021-05-25 15:49:41 +0900

    保存

--- spec/support/request_apec_helper.rb
+++ spec/support/request_apec_helper.rb
@@ -3,14 +3,15 @@ module RequestSpecHelper
   def self.included(base)
     base.before(:each) { Warden.test_mode! }
     base.after(:each) { Warden.test_reset! }
-end
+  end
   def sign_in(resource)
     login_as(resource, scope: warden_scope(resource))
-end
+  end
   def sign_out(resource)
     logout(warden_scope(resource))
-end
+  end
 private
   def warden_scope(resource)
     resource.class.name.underscore.to_sym
-end end
+  end
+end
