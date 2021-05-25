335a545083d27213ff2da3a9bf89234f02c81ebc
lvjiiiiu <lv_jiiiiu@yahoo.co.jp>
2021-05-25 16:22:49 +0900

    タスクmodelスペックの追加

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:project) { FactoryBot.create(:project) }
+
+  # プロジェクトと名前が有効な状態であること
+  it "is valid with a project and name" do
+    task = Task.new(
+      project: project,
+      name: "Test task",
+  )
+    expect(task).to be_valid
+  end
+  # プロジェクトがなければ無効な状態であること
+  it "is invalid without a project" do
+    task = Task.new(project: nil)
+    task.valid?
+    expect(task.errors[:project]).to include("must exist")
+  end
+
+  # 名前がなければ無効な状態であること
+  it "is invalid without a name" do
+    task = Task.new(name: nil)
+    task.valid?
+    expect(task.errors[:name]).to include("can't be blank")
+  end
 end
