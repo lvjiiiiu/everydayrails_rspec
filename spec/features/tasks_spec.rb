748aabe61a9e9fef9c110214c4cb764cccf6cec7
lvjiiiiu <lv_jiiiiu@yahoo.co.jp>
2021-05-27 11:54:21 +0900

    エラー

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -2,27 +2,50 @@ require 'rails_helper'
 
 RSpec.feature "Tasks", type: :feature do
   # ユーザーがタスクの状態を切り替える
+  let(:user) { FactoryBot.create(:user) }
+  let(:project) {
+    FactoryBot.create(:project,
+      name: "RSpec tutorial",
+      owner: user)
+     }
+  let!(:task) { project.tasks.create!(name: "Finish RSpecr tutorial") }
+
   scenario "user toggles a task", js:true do
-    user = FactoryBot.create(:user)
-    project = FactoryBot.create(:project, name: "RSpec tutorial", owner: user)
-    task = project.tasks.create!(name: "Finish RSpecr tutorial")
 
+    sign_in user
+    go_to_project "RSpec tutorial"
+    save_and_open_page
+    complete_task "Finish RSpec tutorial"
+    expect_complete_task "Finish RSpec tutorial"
+    undo_complete_task "Finish RSpec tutorial"
+    expect_incomplete_task "Finish RSpec tutorial"
+
+  end
+
+  def go_to_project(name)
     visit root_path
-    click_link "Sign in"
-    fill_in "Email", with: user.email
-    fill_in "Password", with: user.password
-    click_button "Log in"
-    
-    click_link "RSpec tutorial"
-    check "completed"
-    
+    click_link name
+  end
 
-    expect(page).to have_css "label#task_#{task.id}.completed"
-    expect(task.reload).to be_completed
+  def complete_task(name)
+    check name
+  end
 
-    uncheck "completed"
+  def undo_complete_task(name)
+    uncheck name
+  end
 
-    expect(page).to have_css "label#task_#{task.id}.completed"
-    expect(task.reload).to_not be_completed
+  def expect_complete_task(name)
+    aggregate_failures do
+      expect(page).to have_css "label.completed", text: name
+      expect(task.reload).to be_completed
+    end
+  end
+
+  def expect_incomplete_task(name)
+    aggregate_failures do
+      expect(page).to_not have_css "label.completed", text: name
+      expect(task.reload).to_mot be_completed
+    end
   end
 end
